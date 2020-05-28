9d756b8dfc8589d456001a3f2937bf68ed1a6d96
Bart <bartswebdevelopment@gmail.com>
2020-05-28 19:26:05 +0200

    adding files for github pages

---
+++ build/precache-manifest.4e9b78d6c996dee0935d2f1976e16761.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "176acb4ef131601a254eba5e9c0e39ad",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "046c39be04f901f8c9ea",
+    "url": "/monsters/static/css/main.4f0bdbb2.chunk.css"
+  },
+  {
+    "revision": "a118fcff70fde5bdac91",
+    "url": "/monsters/static/js/2.7bede9e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.7bede9e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "046c39be04f901f8c9ea",
+    "url": "/monsters/static/js/main.183ffdc8.chunk.js"
+  },
+  {
+    "revision": "e20b59329f3960f98de4",
+    "url": "/monsters/static/js/runtime-main.e2917106.js"
+  }
+]);
